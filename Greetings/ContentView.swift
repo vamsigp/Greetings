9ed15af3eaf57510b5d45c7147c73acaff57c783
Vamsi Krishna <Vamsi.KrishnaGP@ivanti.com>
2025-01-10 22:37:22 +0530

    Initial Commit

---
+++ Greetings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Greetings
+//
+//  Created by Vamsi Krishna G P on 10/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Vamsi Krishna G P on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
