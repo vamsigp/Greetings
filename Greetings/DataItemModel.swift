6c95847c9ea2d248353203860b4ee509ec9b366c
Vamsi Krishna <Vamsi.KrishnaGP@ivanti.com>
2025-01-16 21:28:18 +0530

    Append with Arrays

---
+++ Greetings/DataItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  DataItemView.swift
+//  Greetings
+//
+//  Created by Vamsi Krishna G P on 16/01/25.
+//
+
+import SwiftUI
+
+struct DataItemView {
+    let text: String
+    let bgColor: Color
+}
